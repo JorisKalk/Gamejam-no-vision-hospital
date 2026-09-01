fdfd64e63d084f9bd4e696f3378109839a9bbaa9
Joris <549108@student.saxion.nl>
2026-09-01 15:51:16 +0200

    rotation sight cone
    the character now has a rotating test cone for the sight visualization

--- gamejam hospital/Assets/Scripts/Movement/PlayerMovement.cs
+++ gamejam hospital/Assets/Scripts/Movement/PlayerMovement.cs
@@ -7,6 +7,8 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     private float moveSpeed = 10f;
 
+    private Vector2 movementDir = Vector2.zero;
+
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -14,15 +16,33 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
-        Vector2 movementDir = new Vector2(0, 0);
+        movementDir = new Vector2(0, 0);
+        Movement();
+        if (movementDir != Vector2.zero)
+        {
+            RotatePlayer();
+        } 
+    }
 
+    private void Movement()
+    {
         if (Input.GetKey(KeyCode.W)) movementDir.y += 1;
         if (Input.GetKey(KeyCode.S)) movementDir.y -= 1;
         if (Input.GetKey(KeyCode.A)) movementDir.x -= 1;
         if (Input.GetKey(KeyCode.D)) movementDir.x += 1;
 
-        movementDir.Normalize();
+        rb.linearVelocity = movementDir.normalized * moveSpeed;
+    }
 
-        rb.linearVelocity = movementDir * moveSpeed;
+    private void RotatePlayer()
+    {
+        if (movementDir == new Vector2(0, -1)) transform.rotation = Quaternion.Euler(0, 0, 0);
+        else if (movementDir == new Vector2(1, -1)) transform.rotation = Quaternion.Euler(0, 0, 45);
+        else if (movementDir == new Vector2(1, 0)) transform.rotation = Quaternion.Euler(0, 0, 90);
+        else if (movementDir == new Vector2(1, 1)) transform.rotation = Quaternion.Euler(0, 0, 135);
+        else if (movementDir == new Vector2(0, 1)) transform.rotation = Quaternion.Euler(0, 0, 180);
+        else if (movementDir == new Vector2(-1, 1)) transform.rotation = Quaternion.Euler(0, 0, 225);
+        else if (movementDir == new Vector2(-1, 0)) transform.rotation = Quaternion.Euler(0, 0, 270);
+        else if (movementDir == new Vector2(-1, -1)) transform.rotation = Quaternion.Euler(0, 0, 315);
     }
 }

5705a6867c0de457ad26f111f7d778e868234f4e
Joris <549108@student.saxion.nl>
2026-09-01 14:12:04 +0200

    character movement and tile layers

--- gamejam hospital/Assets/Scripts/Movement/PlayerMovement.cs
+++ gamejam hospital/Assets/Scripts/Movement/PlayerMovement.cs
@@ -2,5 +2,27 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    
+    private Rigidbody2D rb;
+
+    [SerializeField]
+    private float moveSpeed = 10f;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        Vector2 movementDir = new Vector2(0, 0);
+
+        if (Input.GetKey(KeyCode.W)) movementDir.y += 1;
+        if (Input.GetKey(KeyCode.S)) movementDir.y -= 1;
+        if (Input.GetKey(KeyCode.A)) movementDir.x -= 1;
+        if (Input.GetKey(KeyCode.D)) movementDir.x += 1;
+
+        movementDir.Normalize();
+
+        rb.linearVelocity = movementDir * moveSpeed;
+    }
 }

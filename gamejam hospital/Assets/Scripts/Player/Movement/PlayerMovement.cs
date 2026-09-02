e69774c00171f91e2e5dadc9470f141b7fef43bb
Joris <549108@student.saxion.nl>
2026-09-02 15:12:25 +0200

    trying to solve issue

--- gamejam hospital/Assets/Scripts/Player/Movement/PlayerMovement.cs
+++ gamejam hospital/Assets/Scripts/Player/Movement/PlayerMovement.cs
@@ -2,10 +2,26 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private enum facingDirection
+    {
+        FRONT,
+        BACK,
+        SIDE
+    }
+
     private Rigidbody2D rb;
 
+    [Header("Animation References")]
+    [SerializeField]
+    private SpriteRenderer sprite;
+    [SerializeField]
+    private Animator anim;
+
+    [Header("Movement Values")]
     [SerializeField]
     private float moveSpeed = 10f;
+    [SerializeField]
+    private Transform rotationTransform;
 
     [Header("Cooldown for teleporting")]
     [SerializeField]
@@ -37,29 +53,34 @@ public class PlayerMovement : MonoBehaviour
         if (movementDir != Vector2.zero)
         {
             RotatePlayer();
-        } 
+        }
+        
+        HandleAnimations();
     }
 
     private void Movement()
     {
-        if (Input.GetKey(KeyCode.W)) movementDir.y += 1;
-        if (Input.GetKey(KeyCode.S)) movementDir.y -= 1;
-        if (Input.GetKey(KeyCode.A)) movementDir.x -= 1;
-        if (Input.GetKey(KeyCode.D)) movementDir.x += 1;
+        if (Input.GetKey(KeyCode.W)) movementDir.y = 1;
+        else if (Input.GetKey(KeyCode.S)) movementDir.y = -1;
+        else movementDir.y = 0;
+
+        if (Input.GetKey(KeyCode.A)) movementDir.x = -1;
+        else if (Input.GetKey(KeyCode.D)) movementDir.x = 1;
+        else movementDir.x = 0;
 
         rb.linearVelocity = movementDir.normalized * moveSpeed;
     }
 
     private void RotatePlayer()
     {
-        if (movementDir == new Vector2(0, -1)) transform.rotation = Quaternion.Euler(0, 0, 0);
-        else if (movementDir == new Vector2(1, -1)) transform.rotation = Quaternion.Euler(0, 0, 45);
-        else if (movementDir == new Vector2(1, 0)) transform.rotation = Quaternion.Euler(0, 0, 90);
-        else if (movementDir == new Vector2(1, 1)) transform.rotation = Quaternion.Euler(0, 0, 135);
-        else if (movementDir == new Vector2(0, 1)) transform.rotation = Quaternion.Euler(0, 0, 180);
-        else if (movementDir == new Vector2(-1, 1)) transform.rotation = Quaternion.Euler(0, 0, 225);
-        else if (movementDir == new Vector2(-1, 0)) transform.rotation = Quaternion.Euler(0, 0, 270);
-        else if (movementDir == new Vector2(-1, -1)) transform.rotation = Quaternion.Euler(0, 0, 315);
+        if (movementDir == new Vector2(0, -1)) rotationTransform.rotation = Quaternion.Euler(0, 0, 0);
+        else if (movementDir == new Vector2(1, -1)) rotationTransform.rotation = Quaternion.Euler(0, 0, 45);
+        else if (movementDir == new Vector2(1, 0)) rotationTransform.rotation = Quaternion.Euler(0, 0, 90);
+        else if (movementDir == new Vector2(1, 1)) rotationTransform.rotation = Quaternion.Euler(0, 0, 135);
+        else if (movementDir == new Vector2(0, 1)) rotationTransform.rotation = Quaternion.Euler(0, 0, 180);
+        else if (movementDir == new Vector2(-1, 1)) rotationTransform.rotation = Quaternion.Euler(0, 0, 225);
+        else if (movementDir == new Vector2(-1, 0)) rotationTransform.rotation = Quaternion.Euler(0, 0, 270);
+        else if (movementDir == new Vector2(-1, -1)) rotationTransform.rotation = Quaternion.Euler(0, 0, 315);
     }
 
     public bool CanTeleport()
@@ -72,5 +93,19 @@ public class PlayerMovement : MonoBehaviour
         transform.position = targetPos.position;
         teleportCooldownLeft = teleportCooldown;
         canTeleport = false;
+    }
+
+
+    private void HandleAnimations()
+    {
+        if (movementDir.x > 0) sprite.flipX = false;
+        else if (movementDir.x < 0) sprite.flipX = true;
+
+        if (movementDir.x != 0) anim.SetInteger("FacingDirection", (int)facingDirection.SIDE);
+        else if (movementDir.y > 0) anim.SetInteger("FacingDirection", (int)facingDirection.BACK);
+        else if (movementDir.y < 0) anim.SetInteger("FacingDirection", (int)facingDirection.FRONT);
+
+        if (movementDir.magnitude != 0) anim.SetBool("Walking", true);
+        else anim.SetBool("Walking", false);
     }
 }
